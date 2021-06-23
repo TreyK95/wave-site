0c0ed7b98b7b0834b301c0cbb2cc83724e5ae496
Trey Kemp <kemptrey@outlook.com>
2021-06-22 21:31:40 -0600

    working on styling

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ document.querySelectorAll(".story-btn").forEach((btn) => {
     btn.nextElementSibling.classList.toggle("change");
   });
 });
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons button i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = ".7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+});
+
+// end of section 2 video
